c5c80ed5523023e0b39a1415dac3509cbfec1d3d
mathias <mathiasd415@gmail.com>
2021-07-02 11:47:40 +0200

    tableau=article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,9 +10,42 @@ class ArticleController
 {
 
     /**
-     * @Route ("/article", name="article")
+     * @Route ("/articles", name="articles")
      */
-    public function articles(){
-        return new Response('article');
+    public function articlesListe()
+    {
+        return new Response('articles');
+    }
+
+    /**
+     * @Route ("/article/{id}", name="articleShow")
+     */
+    public function articleShow($id){
+
+        $articles = [
+            1 => [
+                "title" => "La vaccination c'est trop géniale",
+                "content" => "bablablblalba",
+                "id" => 1
+            ],
+            2 => [
+                "title" => "La vaccination c'est pas trop géniale",
+                "content" => "blablablabla",
+                "id" => 2
+            ],
+            3 => [
+                "title" => "Balkany c'est trop génial",
+                "content" => "balblalblalb",
+                "id" => 3
+            ],
+            4 => [
+                "title" => "Balkany c'est pas trop génial",
+                "content" => "balblalblalb",
+                "id" => 4
+            ]
+        ];
+
+        $articles = $articles[$id];
+        return new Response("Le titre de l'article : ". $articles['title']);
     }
 }
