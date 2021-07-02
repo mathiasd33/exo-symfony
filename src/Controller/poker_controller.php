9bf989605ab24b13e91782744d805d7c465e904c
mathias <mathiasd415@gmail.com>
2021-07-02 09:11:59 +0200

    redirection ->page html

--- src/Controller/poker_controller.php
+++ src/Controller/poker_controller.php
@@ -3,23 +3,39 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class poker_controller
+class poker_controller extends AbstractController
 {
     /**
-     * @Route ("/poker", name="poker")
+     * @Route("/poker", name="poker")
      */
-    public function poker(){
+    public function poker()
+    {
         $request = Request::createFromGlobals();
+
         $age = $request->query->get('age');
 
-        if($age>=18){
-            return new Response("poker en ligne");
+        if ($age >= 18) {
+            return $this->render("index.html");
+        } else {
+            //  redirection vers la route digimon
+            // grâce à la méthode redirectToRoute qui existe dans
+            // l'AbstractController
+            // classe PageController hérite d'AbstractController
+            // et méthode redirectToRoute en herite aussi
+            return $this->render("pokemon.html");
         }
-        return new Response('il faut avoir plus de 18 ans');
     }
+
+    ///**
+    // * @Route("/digimon", name="digimon")
+     //*/
+    //public function digimon()
+    //{
+//  new Response("Les digimons");
+    //}
 
 }
